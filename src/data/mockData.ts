243bd4b4c6981dcab75f2eb499cd9e746ec22ae2
ynzung <syj91105@sookmyung.ac.kr>
2025-07-02 20:47:14 +0900

    chore: 채팅 목록에 쓰일 목데이터 추가(#3)

---
+++ src/data/mockData.ts
@@ -0,0 +1,10 @@
+import type { Contact } from '@/types/support';
+
+export const contacts: Contact[] = [
+  { chat_id: 1, name: '아이디', phone: '010-1234-5678', time: '3분 전' },
+  { chat_id: 2, name: '비회원', phone: '010-1234-5678', time: '12월 31일' },
+  { chat_id: 3, name: '비회원', phone: '010-1234-5678', time: '12월 31일' },
+  { chat_id: 4, name: '비회원', phone: '010-1234-5678', time: '12월 31일' },
+  { chat_id: 5, name: '비회원', phone: '010-1234-5678', time: '12월 31일' },
+  { chat_id: 6, name: '비회원', phone: '010-1234-5678', time: '12월 31일' },
+];
