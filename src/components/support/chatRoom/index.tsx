e0c811a2ddfce909c650167cccb4a820e47dae4b
ynzung <syj91105@sookmyung.ac.kr>
2025-07-04 02:17:48 +0900

    feat: ChatRoomPanel에 ChatHeader 컴포넌트 연결 (#11)

--- src/components/support/chatRoom/index.tsx
+++ src/components/support/chatRoom/index.tsx
@@ -1,8 +1,10 @@
+import ChatHeader from './ChatHeader';
 import ChatRoom from './ChatRoom';
 
 const ChatRoomPanel = () => {
   return (
-    <div className="flex flex-col w-[35%] gap-1 border-r border-lineGray bg-bgLightBlue overflow-y-auto">
+    <div className="flex flex-col w-[35%] border-r border-lineGray bg-bgLightBlue">
+      <ChatHeader />
       <ChatRoom />
     </div>
   );
