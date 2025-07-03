f23edb782208767c7cf168abcfcb53c4d380dd84
ynzung <syj91105@sookmyung.ac.kr>
2025-07-04 02:17:12 +0900

    feat: ChatRoom에 ChatHeader 연결 및 자동 하단 스크롤 구현 (#11)

--- src/components/support/chatRoom/ChatRoom.tsx
+++ src/components/support/chatRoom/ChatRoom.tsx
@@ -1,10 +1,11 @@
 import ClientBubble from './ClientBubble';
 import AgentBubble from './AgentBubble';
 import { MessageType } from '@/types/support';
+import { useRef, useEffect, useState } from 'react';
 
 const MOCK_MESSAGE: MessageType[] = [
   {
-    id: 1,
+    messageId: 1,
     senderType: 'client',
     original:
       '안녕하세요! 무엇을 도와드릴까요?안녕하세요! 무엇을 도와드릴까요? 안녕하세요! 무엇을 도와드릴까요?',
@@ -12,44 +13,98 @@ const MOCK_MESSAGE: MessageType[] = [
     timestamp: '2025-01-01 12:00:00',
   },
   {
-    id: 2,
+    messageId: 2,
     senderType: 'user',
     original: '안녕하세요! 무엇을 도와드릴까요?',
     timestamp: '2025-01-01 12:00:00',
   },
   {
-    id: 3,
+    messageId: 3,
     senderType: 'client',
     original:
       '안녕하세요! 무엇을 도와드릴까요?안녕하세요! 무엇을 도와드릴까요?안녕하세요! 무엇을 도와드릴까요?',
     timestamp: '2025-01-01 12:00:00',
   },
   {
-    id: 4,
+    messageId: 4,
     senderType: 'user',
     original: '안녕하세요! 무엇을 도와드릴까요?',
     timestamp: '2025-01-01 12:00:00',
   },
   {
-    id: 5,
+    messageId: 5,
     senderType: 'user',
     original: '안녕하세요! 무엇을 도와드릴까요?',
     timestamp: '2025-01-01 12:00:00',
   },
   {
-    id: 6,
+    messageId: 6,
     senderType: 'client',
     original: '원문입니다 불손불손불손',
     polite: '안녕하세요! 무엇을 도와드릴까요?',
     timestamp: '2025-01-01 12:00:00',
   },
+  {
+    messageId: 7,
+    senderType: 'client',
+    original: '원문입니다 불손불손불손',
+    polite: '안녕하세요! 무엇을 도와드릴까요?',
+    timestamp: '2025-01-01 12:00:00',
+  },
+  {
+    messageId: 8,
+    senderType: 'client',
+    original: '안녕하세요! 무엇을 도와드릴까요?',
+    timestamp: '2025-01-01 12:00:00',
+  },
+  {
+    messageId: 9,
+    senderType: 'client',
+    original: '안녕하세요! 무엇을 도와드릴까요?',
+    timestamp: '2025-01-01 12:00:00',
+  },
+  {
+    messageId: 10,
+    senderType: 'client',
+    original: '안녕하세요! 무엇을 도와드릴까요?',
+    timestamp: '2025-01-01 12:00:00',
+  },
+  {
+    messageId: 11,
+    senderType: 'user',
+    original: '안녕하세요! 무엇을 도와드릴까요?',
+    timestamp: '2025-01-01 12:00:00',
+  },
+  {
+    messageId: 12,
+    senderType: 'user',
+    original: '안녕하세요! 무엇을 도와드릴까요?',
+    timestamp: '2025-01-01 12:00:00',
+  },
+  {
+    messageId: 13,
+    senderType: 'user',
+    original: '안녕하세요! 무엇을 도와드릴까요?',
+    timestamp: '2025-01-01 12:00:00',
+  },
 ];
 
 export default function ChatRoom() {
+  const [messages] = useState(MOCK_MESSAGE);
+  const chatEndRef = useRef<HTMLDivElement | null>(null);
+
+  const scrollToBottom = () => {
+    chatEndRef.current?.scrollIntoView({ behavior: 'smooth' });
+  };
+
+  useEffect(() => {
+    scrollToBottom();
+  }, [messages]);
+
   return (
-    <div className="flex flex-col gap-2 p-5">
-      {MOCK_MESSAGE.map((message) => (
-        <div key={message.id}>
+    <div className="flex flex-col gap-2 p-5 overflow-y-auto">
+      {messages.map((message) => (
+        <div key={message.messageId}>
           {message.senderType === 'client' ? (
             <ClientBubble message={message} />
           ) : (
@@ -57,6 +112,7 @@ export default function ChatRoom() {
           )}
         </div>
       ))}
+      <div ref={chatEndRef} />
     </div>
   );
 }
