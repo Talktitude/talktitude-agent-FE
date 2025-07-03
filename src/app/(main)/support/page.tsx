d58052020176915d3d9418ec73a2daef2d00e1d7
ynzung <syj91105@sookmyung.ac.kr>
2025-07-04 01:11:35 +0900

    refactor: SupportPage 내 상담 목록 및 채팅 영역 컴포넌트 분리 (#9)

--- src/app/(main)/support/page.tsx
+++ src/app/(main)/support/page.tsx
@@ -1,21 +1,17 @@
 'use client';
 
 import Header from '@/components/common/Header';
-import ChatList from '@/components/support/ChatList';
-import SearchInput from '@/components/support/SearchInput';
-import SectionHeader from '@/components/support/SectionHeader';
+import ChatListPanel from '@/components/support/chatList';
+import ChatRoomPanel from '@/components/support/chatRoom';
 import React from 'react';
 
 export default function SupportPage() {
   return (
     <>
       <Header />
-      <div className="flex w-full">
-        <div className="flex flex-col flex-1 gap-1 border-r border-lineGrey">
-          <SectionHeader title="상담 목록" />
-          <SearchInput />
-          <ChatList />
-        </div>
+      <div className="flex w-full h-[calc(100vh-60px)]">
+        <ChatListPanel />
+        <ChatRoomPanel />
       </div>
     </>
   );
