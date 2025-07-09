d1476c082e4f7f77fa426876db3e0afdc6795736
ynzung <syj91105@sookmyung.ac.kr>
2025-07-10 00:58:39 +0900

    feat: SupportInfoPanel에 RecommendationPanel 가져오기 (#23)

--- src/components/support/supportInfo/SupportInfoPanel.tsx
+++ src/components/support/supportInfo/SupportInfoPanel.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import InfoPanel from './info/InfoPanel';
+import RecommendationPanel from './recommendation/RecommendationPanel';
 
 const SupportInfoPanel = () => {
   return (
     <div className="flex flex-col w-[35%] border-r border-lineGray bg-white">
       <InfoPanel />
+      <RecommendationPanel />
     </div>
   );
 };
