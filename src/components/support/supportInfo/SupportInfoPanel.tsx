743463c847ea01f67841c38ae72cc81e528c2897
ynzung <syj91105@sookmyung.ac.kr>
2025-07-10 16:34:52 +0900

    feat: SupportInfoPanel에 setInputMessage prop 추가 (#23)

--- src/components/support/supportInfo/SupportInfoPanel.tsx
+++ src/components/support/supportInfo/SupportInfoPanel.tsx
@@ -2,11 +2,15 @@ import React from 'react';
 import InfoPanel from './info/InfoPanel';
 import RecommendationPanel from './recommendation/RecommendationPanel';
 
-const SupportInfoPanel = () => {
+interface SupportInfoPanelProps {
+  setInputMessage?: (message: string) => void;
+}
+
+const SupportInfoPanel = ({ setInputMessage }: SupportInfoPanelProps) => {
   return (
     <div className="flex flex-col w-[35%] border-r border-lineGray bg-white">
       <InfoPanel />
-      <RecommendationPanel />
+      <RecommendationPanel setInputMessage={setInputMessage} />
     </div>
   );
 };
