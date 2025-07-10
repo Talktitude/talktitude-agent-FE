db0129ba4bc35e9e6cb06ef29c6034bc624311fb
ynzung <syj91105@sookmyung.ac.kr>
2025-07-10 16:36:44 +0900

    feat: 메인 페이지에서 inputMessage 상태 관리하도록 설정 (#23)

--- src/app/(main)/support/page.tsx
+++ src/app/(main)/support/page.tsx
@@ -1,19 +1,24 @@
 'use client';
 
-import React from 'react';
+import React, { useState } from 'react';
 import Header from '@/components/common/Header';
 import ChatListPanel from '@/components/support/chatList/ChatListPanel';
 import ChatRoomPanel from '@/components/support/chatRoom/ChatRoomPanel';
 import SupportInfoPanel from '@/components/support/supportInfo/SupportInfoPanel';
 
 export default function SupportPage() {
+  const [inputMessage, setInputMessage] = useState('');
+
   return (
     <>
       <Header />
       <div className="flex w-full h-[calc(100vh-60px)]">
         <ChatListPanel />
-        <ChatRoomPanel />
-        <SupportInfoPanel />
+        <ChatRoomPanel
+          inputMessage={inputMessage}
+          setInputMessage={setInputMessage}
+        />
+        <SupportInfoPanel setInputMessage={setInputMessage} />
       </div>
     </>
   );
