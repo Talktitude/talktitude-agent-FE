3aa0ab72ce799bbb67255b3a4695217f733ca65f
ynzung <syj91105@sookmyung.ac.kr>
2025-07-16 18:57:40 +0900

    feat: ReportPage 생성 (#27)

---
+++ src/app/(main)/reports/page.tsx
@@ -0,0 +1,27 @@
+'use client';
+
+import React from 'react';
+import Header from '@/components/common/Header';
+import { Calendar } from '@/components/ui/calendar';
+
+export default function ReportPage() {
+  const [date, setDate] = React.useState<Date | undefined>(new Date());
+
+  return (
+    <>
+      <Header />
+      <div className="flex flex-col items-center justify-center w-[50%] h-[calc(100dvh-60px)] bg-bgLightBlue">
+        <Calendar
+          onDayClick={(date) => {
+            const formatted = date.toISOString().slice(0, 10);
+            console.log(formatted);
+          }}
+          mode="single"
+          selected={date}
+          onSelect={setDate}
+          className="rounded-lg border"
+        />
+      </div>
+    </>
+  );
+}
