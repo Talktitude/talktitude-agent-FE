1a6833df096d14dedc6985aecb42f28fb41db192
ynzung <syj91105@sookmyung.ac.kr>
2025-07-17 20:57:10 +0900

    chore: 리포트 목록 type 설정 (#29)

---
+++ src/types/reports.ts
@@ -0,0 +1,10 @@
+export interface ReportItemType {
+  reportId: number;
+  sessionId: number;
+  clientName: string;
+  phone: string;
+  createdAt: string;
+  category: string;
+  clientId: string;
+  profileImage: string;
+}
