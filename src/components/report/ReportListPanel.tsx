63aeba3adf64e75ed5da921483ed87259c4c4239
ynzung <syj91105@sookmyung.ac.kr>
2025-07-17 21:30:19 +0900

    design: ReportListPanel 컴포넌트 너비 설정 (#29)

--- src/components/report/ReportListPanel.tsx
+++ src/components/report/ReportListPanel.tsx
@@ -3,7 +3,7 @@ import ReportList from './ReportList';
 
 const ReportListPanel = () => {
   return (
-    <div className="flex flex-col overflow-y-auto h-full">
+    <div className="flex flex-col overflow-y-auto h-full w-[50%] bg-white">
       <ReportList />
     </div>
   );
