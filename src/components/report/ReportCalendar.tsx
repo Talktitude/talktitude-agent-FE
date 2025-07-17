4a23c72dcddb5c7d503f69e3f969cbca4100eff5
ynzung <syj91105@sookmyung.ac.kr>
2025-07-17 23:44:37 +0900

    feat: report 페이지 새로고침 시 항상 오늘 날짜로 url 변경되도록 설정 (#31)

--- src/components/report/ReportCalendar.tsx
+++ src/components/report/ReportCalendar.tsx
@@ -1,10 +1,21 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Calendar } from '@/components/ui/calendar';
 import { useRouter } from 'next/navigation';
 
 const ReportCalendar = () => {
   const router = useRouter();
   const [date, setDate] = React.useState<Date | undefined>(new Date());
+
+  // 새로고침 시 항상 오늘 날짜로 URL 변경
+  useEffect(() => {
+    const today = new Date();
+    const formatted = today.toLocaleDateString('sv-SE', {
+      timeZone: 'Asia/Seoul',
+    });
+    router.replace(`/reports?date=${formatted}`);
+    setDate(today);
+  }, [router]);
+
   return (
     <div className="flex items-center justify-center w-full md:w-[50%] bg-bgLightBlue p-0 md:p-0 lg:p-8">
       <Calendar
