52893ee2186f7ec4580642fff8451d863fa53368
ynzung <syj91105@sookmyung.ac.kr>
2025-07-17 22:41:53 +0900

    feat: 리포트 목록 패널에 SearchInput 연동 및 검색 입력 상태 관리 추가 (#31)

--- src/components/report/ReportListPanel.tsx
+++ src/components/report/ReportListPanel.tsx
@@ -1,9 +1,27 @@
-import React from 'react';
+'use client';
+
+import React, { useState } from 'react';
 import ReportList from './ReportList';
+import SearchInput from '@/components/common/SearchInput';
 
 const ReportListPanel = () => {
+  const [searchValue, setSearchValue] = useState('');
+  const handleSearchReport = () => {
+    console.log(searchValue);
+    setSearchValue('');
+  };
+  const handleChangeReport = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setSearchValue(e.target.value);
+    // console.log(searchValue);
+  };
   return (
-    <div className="flex flex-col overflow-y-auto h-full w-[50%] bg-white">
+    <div className="flex flex-col overflow-y-auto h-full w-[50%] bg-white p-4">
+      <SearchInput
+        onSearchClick={() => {
+          handleSearchReport();
+        }}
+        onChange={handleChangeReport}
+      />
       <ReportList />
     </div>
   );
