890b26cd650654ee434867f34a8f46776f00642c
ynzung <syj91105@sookmyung.ac.kr>
2025-07-19 23:58:52 +0900

    chore: 리포트 상세보기 type 선언 (#33)

--- src/types/reports.ts
+++ src/types/reports.ts
@@ -2,9 +2,21 @@ export interface ReportItemType {
   reportId: number;
   sessionId: number;
   clientName: string;
-  phone: string;
+  clientPhone: string;
   createdAt: string;
   category: string;
   clientId: string;
-  profileImage: string;
+  profileImageUrl: string;
+}
+
+export interface ReportDetailType {
+  reportId: number;
+  sessionId: number;
+  clientName: string;
+  clientPhone: string;
+  createdAt: string;
+  userName: string;
+  category: string;
+  summaryText: string;
+  profileImageUrl: string;
 }
