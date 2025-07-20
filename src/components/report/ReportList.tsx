c35371029035a979ab203304f958b5ba513b97da
ynzung <syj91105@sookmyung.ac.kr>
2025-07-21 01:07:32 +0900

    feat: ReportList 데이터 없는 경우 렌더링 문구 추가 (#33)

--- src/components/report/ReportList.tsx
+++ src/components/report/ReportList.tsx
@@ -121,53 +121,63 @@ const ReportList = () => {
   };
 
   return (
-    <div className="flex-1 flex flex-col">
-      {paginatedItems.map((reportItem) => (
-        <ReportItem
-          reportItem={reportItem}
-          key={reportItem.reportId}
-          onClick={() =>
-            router.push(`/reports/${reportItem.reportId}?date=${date}`)
-          }
-        />
-      ))}
-      <div className="flex justify-center mt-3">
-        <Pagination>
-          <PaginationContent>
-            <PaginationItem>
-              <PaginationPrevious
-                onClick={() => handlePageChange(currentPage - 1)}
-                style={{
-                  cursor: currentPage === 1 ? 'not-allowed' : 'pointer',
-                }}
-                aria-disabled={currentPage === 1}
-              />
-            </PaginationItem>
-            {Array.from({ length: totalPages }, (_, idx) => (
-              <PaginationItem key={idx + 1}>
-                <PaginationLink
-                  isActive={currentPage === idx + 1}
-                  onClick={() => handlePageChange(idx + 1)}
-                  style={{ cursor: 'pointer' }}
-                >
-                  {idx + 1}
-                </PaginationLink>
-              </PaginationItem>
-            ))}
-            <PaginationItem>
-              <PaginationNext
-                onClick={() => handlePageChange(currentPage + 1)}
-                style={{
-                  cursor:
-                    currentPage === totalPages ? 'not-allowed' : 'pointer',
-                }}
-                aria-disabled={currentPage === totalPages}
-              />
-            </PaginationItem>
-          </PaginationContent>
-        </Pagination>
-      </div>
-    </div>
+    <>
+      {paginatedItems.length > 0 ? (
+        <div className="flex-1 flex flex-col">
+          {paginatedItems.map((reportItem) => (
+            <ReportItem
+              reportItem={reportItem}
+              key={reportItem.reportId}
+              onClick={() =>
+                router.push(`/reports/${reportItem.reportId}?date=${date}`)
+              }
+            />
+          ))}
+          <div className="flex justify-center mt-3">
+            <Pagination>
+              <PaginationContent>
+                <PaginationItem>
+                  <PaginationPrevious
+                    onClick={() => handlePageChange(currentPage - 1)}
+                    style={{
+                      cursor: currentPage === 1 ? 'not-allowed' : 'pointer',
+                    }}
+                    aria-disabled={currentPage === 1}
+                  />
+                </PaginationItem>
+                {Array.from({ length: totalPages }, (_, idx) => (
+                  <PaginationItem key={idx + 1}>
+                    <PaginationLink
+                      isActive={currentPage === idx + 1}
+                      onClick={() => handlePageChange(idx + 1)}
+                      style={{ cursor: 'pointer' }}
+                    >
+                      {idx + 1}
+                    </PaginationLink>
+                  </PaginationItem>
+                ))}
+                <PaginationItem>
+                  <PaginationNext
+                    onClick={() => handlePageChange(currentPage + 1)}
+                    style={{
+                      cursor:
+                        currentPage === totalPages ? 'not-allowed' : 'pointer',
+                    }}
+                    aria-disabled={currentPage === totalPages}
+                  />
+                </PaginationItem>
+              </PaginationContent>
+            </Pagination>
+          </div>
+        </div>
+      ) : (
+        <div className="flex-1 flex flex-col items-center justify-center">
+          <div className="text-textBlack text-base font-medium">
+            해당 날짜에 상담 내역이 없습니다.
+          </div>
+        </div>
+      )}
+    </>
   );
 };
 
