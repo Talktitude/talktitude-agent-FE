ae32bedad6151d875e49252e2652737f6287f916
ynzung <syj91105@sookmyung.ac.kr>
2025-07-31 00:58:53 +0900

    design: InputField 컴포넌트 스타일 수정 (#37)

--- src/components/auth/InputField.tsx
+++ src/components/auth/InputField.tsx
@@ -12,14 +12,16 @@ function InputField({
   return (
     <div className="flex-1 flex flex-col gap-1.5">
       {isSignup && (
-        <label className="text-lg font-bold text-textBlack">{inputLabel}</label>
+        <label className="text-base font-bold text-textBlack">
+          {inputLabel}
+        </label>
       )}
       <input
         type={type}
         placeholder={placeholder}
         value={value}
         onChange={onChange}
-        className="w-full h-12 px-5 py-3.5 text-textBlack text-base font-medium outline-none shadow-inputShadow rounded-[20px] border-[1px] border-lineGray focus:border-[1px] focus:border-mainColor"
+        className="w-full h-12 px-5 py-3.5 text-textBlack text-sm font-medium outline-none shadow-inputShadow rounded-[20px] border-[1px] border-lineGray focus:border-[1px] focus:border-mainColor"
       />
     </div>
   );
