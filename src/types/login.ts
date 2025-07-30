a3b3f2d11b9896e058339fae1f0fe9cde7064900
ynzung <syj91105@sookmyung.ac.kr>
2025-07-30 14:42:10 +0900

    chore: 로그인 type 정의 (#35)

---
+++ src/types/login.ts
@@ -0,0 +1,32 @@
+export interface LoginFormPropsType {
+  loginId: string;
+  password: string;
+  onIdChange: (e: React.ChangeEvent<HTMLInputElement>) => void; // 로그인 아이디 유효성 검사 함수 연결
+  onPasswordChange: (e: React.ChangeEvent<HTMLInputElement>) => void; // 비밀번호 유효성 검사 함수 연결
+  onSubmit: (e: React.FormEvent<HTMLFormElement>) => void; // 로그인 폼 제출 함수 연결
+  keepLoggedIn: boolean; // 로그인 유지 체크박스 상태
+  handleKeepLoggedInClick: () => void; // 로그인 유지 토큰 설정 로직 함수 연결
+  disabled: boolean; // 로그인 버튼 비활성화 상태
+  isEmptyLogin: boolean; // 로그인 아이디와 비밀번호 입력 값이 없을 때 에러 상태
+  loginErrorMessage: string; // 로그인 에러 메시지
+}
+
+export interface InputFieldPropsType {
+  placeholder: string; // 입력 필드 플레이스홀더
+  type: string; // 입력 필드 타입
+  value: string; // 입력 필드 값
+  onChange: (e: React.ChangeEvent<HTMLInputElement>) => void; // 입력 필드 값 변경 함수
+}
+
+export interface BottomButtonPropsType {
+  type?: 'submit' | 'button'; // 버튼 타입
+  disabled?: boolean; // 버튼 비활성화 상태
+  className?: string; // 버튼 클래스 이름
+  onClick?: () => void; // 버튼 클릭 함수
+  children: React.ReactNode; // 버튼 자식 요소
+}
+
+export interface RememberBoxPropsType {
+  keepLoggedIn: boolean; // 로그인 유지 체크박스 상태
+  handleKeepLoggedInClick: () => void; // 로그인 유지 토큰 설정 로직 함수 연결
+}
