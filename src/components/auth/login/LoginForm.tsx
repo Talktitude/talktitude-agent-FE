a302894bc832c56a2dbf92509d04ff7b28b9e29a
ynzung <syj91105@sookmyung.ac.kr>
2025-07-31 00:58:45 +0900

    chore: 로그인 placeholder 상수 이름을 LOGIN_PLACEHOLDERS로 변경 (#37)

--- src/components/auth/login/LoginForm.tsx
+++ src/components/auth/login/LoginForm.tsx
@@ -3,7 +3,7 @@ import { LoginFormPropsType } from '@/types/auth';
 import InputField from '../InputField';
 import BottomButton from '../BottomButton';
 import RememberBox from './RememberBox';
-import { PLACEHOLDERS } from '@/lib/constants/placeholders';
+import { LOGIN_PLACEHOLDERS } from '@/lib/constants/placeholders';
 
 const LoginForm = ({
   loginFormData,
@@ -18,13 +18,13 @@ const LoginForm = ({
     <div className="w-full max-w-[420px]">
       <form onSubmit={onSubmit} className="flex flex-col gap-4">
         <InputField
-          placeholder={PLACEHOLDERS.ID_INPUT}
+          placeholder={LOGIN_PLACEHOLDERS.ID_INPUT}
           type="text"
           value={loginFormData.loginId}
           onChange={onLoginChange('loginId')}
         />
         <InputField
-          placeholder={PLACEHOLDERS.PW_INPUT}
+          placeholder={LOGIN_PLACEHOLDERS.PW_INPUT}
           type="password"
           value={loginFormData.password}
           onChange={onLoginChange('password')}
@@ -35,7 +35,7 @@ const LoginForm = ({
           handleKeepLoggedInClick={handleKeepLoggedInClick}
         />
         {disabled && (
-          <p className="text-textRed text-sm font-semibold">
+          <p className="text-textRed text-[13px] font-semibold">
             {loginErrorMessage}
           </p>
         )}
