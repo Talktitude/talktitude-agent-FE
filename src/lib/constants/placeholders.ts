8b810f1bcc14f7ecf3d6fb56845a305de7f038e1
ynzung <syj91105@sookmyung.ac.kr>
2025-07-30 14:41:31 +0900

    style: placeholder 문구 일부 수정 (#35)

--- src/lib/constants/placeholders.ts
+++ src/lib/constants/placeholders.ts
@@ -1,8 +1,8 @@
 export const PLACEHOLDERS = {
-  ID_INPUT: '아이디를 입력해 주세요',
-  PW_INPUT: '비밀번호를 입력해 주세요',
-  CHAT_INPUT: '메시지를 입력해 주세요',
+  ID_INPUT: '아이디',
+  PW_INPUT: '비밀번호',
+  CHAT_INPUT: '메시지 입력',
   CLIENT_SEARCH: '고객 검색',
-  CHAT_MEMO: '상담 중 메모를 입력해 주세요',
-  MEMO_INPUT: '상담 내용이나 특이사항을 입력해 주세요.',
+  CHAT_MEMO: '상담 중 메모 입력',
+  MEMO_INPUT: '상담 내용이나 특이사항 입력',
 };
