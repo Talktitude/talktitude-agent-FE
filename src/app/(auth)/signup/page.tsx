0f3aeca799cb1dd23d01beca8f9ac2ee3b65177c
ynzung <syj91105@sookmyung.ac.kr>
2025-07-31 13:35:13 +0900

    design: 회원가입 페이지 상단바 고정 (#37)

--- src/app/(auth)/signup/page.tsx
+++ src/app/(auth)/signup/page.tsx
@@ -8,24 +8,30 @@ import { useSignupForm } from '@/hooks/auth/useSignupForm';
 export default function SignupPage() {
   const {
     signupFormData,
+    errors,
     disabled,
     handleSignupChange,
+    handleBlur,
     handleSubmit,
     handleCheckId,
   } = useSignupForm();
   return (
-    <>
-      <Header />
-      <div className="h-[calc(100vh-49px)] flex flex-col items-center justify-center gap-10">
+    <div className="flex flex-col min-h-screen">
+      <div className="shrink-0 sticky top-0">
+        <Header />
+      </div>
+      <main className="flex-1 overflow-y-auto flex flex-col items-center justify-center gap-10 py-10">
         <h1 className="text-2xl font-bold text-textBlack">회원가입</h1>
         <SignupForm
           signupFormData={signupFormData}
+          errors={errors}
           onSubmit={handleSubmit}
           onSignupChange={handleSignupChange}
+          handleBlur={handleBlur}
           handleCheckId={handleCheckId}
           disabled={disabled}
         />
-      </div>
-    </>
+      </main>
+    </div>
   );
 }
