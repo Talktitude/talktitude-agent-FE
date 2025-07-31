966180d22c14f2d5132e564be999ff4028fc8383
ynzung <syj91105@sookmyung.ac.kr>
2025-07-31 13:32:46 +0900

    chore: 회원가입 에러 메시지 내용 수정 (#37)

--- src/lib/constants/errorMessages.ts
+++ src/lib/constants/errorMessages.ts
@@ -4,8 +4,9 @@ export const LOGIN_ERROR_MESSAGES = {
 };
 
 export const SIGNUP_ERROR_MESSAGES = {
-  EMPTY_SIGNUP: '모든 항목을 입력해주세요.',
+  EMPTY_SIGNUP: '필수 입력 항목입니다.',
   DUPLICATE_ID: '이미 사용 중인 아이디입니다.',
+  ID_NOT_CHECKED: '아이디 중복 확인을 해주세요.',
   WEAK_PASSWORD: '비밀번호는 8~20자의 영문, 숫자, 특수문자를 포함해야 합니다.',
   INVALID_PASSWORD: '비밀번호가 일치하지 않습니다.',
   INVALID_NAME: '이름은 2자 이상 입력해주세요.',
