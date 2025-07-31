681a607d320a95445f74501864c379a32a38fc59
ynzung <syj91105@sookmyung.ac.kr>
2025-07-31 13:34:51 +0900

    feat: SignupForm 컴포넌트 유효성 검사 후 에러 띄우기 기능 추가 (#37)

--- src/components/auth/signup/SignupForm.tsx
+++ src/components/auth/signup/SignupForm.tsx
@@ -6,70 +6,76 @@ import BottomButton from '../BottomButton';
 
 const SignupForm = ({
   signupFormData,
+  errors,
   onSubmit,
   onSignupChange,
+  handleBlur,
   handleCheckId,
   disabled,
 }: SignupFormPropsType) => {
   return (
     <div className="w-full max-w-[420px]">
       <form onSubmit={onSubmit} className="flex flex-col gap-3">
-        <div className="flex flex-row items-end gap-1.5 w-full">
-          <InputField
-            placeholder={SIGNUP_PLACEHOLDERS.ID_INPUT}
-            type="text"
-            value={signupFormData.loginId}
-            onChange={onSignupChange('loginId')}
-            isSignup
-            inputLabel="아이디"
-          />
-          <button
-            className="h-12 bg-mainColor text-base font-semibold text-white rounded-[20px] px-5 py-2 justify-center items-center"
-            onClick={() => handleCheckId(signupFormData.loginId)}
-            type="button"
-          >
-            중복 확인
-          </button>
-        </div>
+        <InputField
+          placeholder={SIGNUP_PLACEHOLDERS.ID_INPUT}
+          type="text"
+          value={signupFormData.loginId}
+          onChange={onSignupChange('loginId')}
+          onBlur={handleBlur('loginId')}
+          isSignup
+          inputLabel="아이디"
+          errorMessage={errors?.loginId}
+          handleCheckId={() => handleCheckId(signupFormData.loginId)}
+        />
         <InputField
           placeholder={SIGNUP_PLACEHOLDERS.PW_INPUT}
           type="password"
           value={signupFormData.password}
           onChange={onSignupChange('password')}
+          onBlur={handleBlur('password')}
           isSignup
           inputLabel="비밀번호"
+          errorMessage={errors?.password}
         />
         <InputField
           placeholder={SIGNUP_PLACEHOLDERS.PW_CONFIRM_INPUT}
           type="password"
           value={signupFormData.password1}
           onChange={onSignupChange('password1')}
+          onBlur={handleBlur('password1')}
           isSignup
           inputLabel="비밀번호 확인"
+          errorMessage={errors?.password1}
         />
         <InputField
           placeholder={SIGNUP_PLACEHOLDERS.NAME_INPUT}
           type="text"
           value={signupFormData.name}
           onChange={onSignupChange('name')}
+          onBlur={handleBlur('name')}
           isSignup
           inputLabel="이름"
+          errorMessage={errors?.name}
         />
         <InputField
           placeholder={SIGNUP_PLACEHOLDERS.PHONE_INPUT}
           type="text"
           value={signupFormData.phone}
           onChange={onSignupChange('phone')}
+          onBlur={handleBlur('phone')}
           isSignup
           inputLabel="전화번호"
+          errorMessage={errors?.phone}
         />
         <InputField
           placeholder={SIGNUP_PLACEHOLDERS.EMAIL_INPUT}
           type="email"
           value={signupFormData.email}
           onChange={onSignupChange('email')}
+          onBlur={handleBlur('email')}
           isSignup
           inputLabel="이메일"
+          errorMessage={errors?.email}
         />
         <div className="pt-6">
           <BottomButton type="submit" disabled={disabled}>
