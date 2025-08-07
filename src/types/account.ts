3e5ca484207103b485bb6420805fe0cb82372acd
ynzung <syj91105@sookmyung.ac.kr>
2025-08-07 14:47:07 +0900

    feat: 비밀번호 변경 관련 type 선언 (#41)

--- src/types/account.ts
+++ src/types/account.ts
@@ -8,9 +8,22 @@ export interface EditFormPropsType {
   onEditChange: (
     key: string,
   ) => (e: React.ChangeEvent<HTMLInputElement>) => void;
+  onEditSubmit: (e: React.FormEvent<HTMLFormElement>) => void;
 }
 
 export interface ProfileImagePropsType {
   profileImageUrl: string;
   onChangePhoto: () => void;
+}
+
+export interface ChangePasswordFormPropsType {
+  passwordData: {
+    currentPassword: string;
+    newPassword: string;
+    newPasswordConfirm: string;
+  };
+  onPasswordChange: (
+    key: string,
+  ) => (e: React.ChangeEvent<HTMLInputElement>) => void;
+  onChangePasswordSubmit: (e: React.FormEvent<HTMLFormElement>) => void;
 }
