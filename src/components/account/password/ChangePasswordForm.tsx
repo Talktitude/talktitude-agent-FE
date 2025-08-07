0468c497d25048e8414163a8ede79bf1a3f41c3f
ynzung <syj91105@sookmyung.ac.kr>
2025-08-07 15:52:28 +0900

    feat: 비밀번호 번경 InputField에 에러 메시지 props 설정 (#41)

--- src/components/account/password/ChangePasswordForm.tsx
+++ src/components/account/password/ChangePasswordForm.tsx
@@ -4,6 +4,7 @@ import BottomButton from '@/components/auth/BottomButton';
 
 const ChangePasswordForm = ({
   passwordData,
+  errors,
   onPasswordChange,
   onChangePasswordSubmit,
 }: ChangePasswordFormPropsType) => {
@@ -12,6 +13,8 @@ const ChangePasswordForm = ({
     !passwordData.newPassword ||
     !passwordData.newPasswordConfirm;
 
+  const hasErrors = Object.values(errors).some((error) => error !== '');
+
   return (
     <div className="w-full max-w-[420px] mx-auto py-6 h-[calc(100vh-120px)]">
       <form
@@ -19,33 +22,42 @@ const ChangePasswordForm = ({
         onSubmit={onChangePasswordSubmit}
       >
         <div className="flex flex-col gap-4">
-          <InputField
-            type="password"
-            value={passwordData.currentPassword}
-            onChange={onPasswordChange('currentPassword')}
-            inputLabel="현재 비밀번호"
-            placeholder="현재 비밀번호를 입력해주세요."
-            isSignup
-          />
-          <InputField
-            type="password"
-            value={passwordData.newPassword}
-            onChange={onPasswordChange('newPassword')}
-            inputLabel="새 비밀번호"
-            placeholder="새 비밀번호를 입력해주세요."
-            isSignup
-          />
-          <InputField
-            type="password"
-            value={passwordData.newPasswordConfirm}
-            onChange={onPasswordChange('newPasswordConfirm')}
-            inputLabel="새 비밀번호 확인"
-            placeholder="새 비밀번호를 다시 입력해주세요."
-            isSignup
-          />
+          <div>
+            <InputField
+              type="password"
+              value={passwordData.currentPassword}
+              onChange={onPasswordChange('currentPassword')}
+              inputLabel="현재 비밀번호"
+              placeholder="현재 비밀번호를 입력해주세요."
+              isSignup
+              errorMessage={errors.currentPassword}
+            />
+          </div>
+          <div>
+            <InputField
+              type="password"
+              value={passwordData.newPassword}
+              onChange={onPasswordChange('newPassword')}
+              inputLabel="새 비밀번호"
+              placeholder="새 비밀번호를 입력해주세요."
+              isSignup
+              errorMessage={errors.newPassword}
+            />
+          </div>
+          <div>
+            <InputField
+              type="password"
+              value={passwordData.newPasswordConfirm}
+              onChange={onPasswordChange('newPasswordConfirm')}
+              inputLabel="새 비밀번호 확인"
+              placeholder="새 비밀번호를 다시 입력해주세요."
+              isSignup
+              errorMessage={errors.newPasswordConfirm}
+            />
+          </div>
         </div>
         <div className="mt-8">
-          <BottomButton type="submit" disabled={isPasswordEmpty}>
+          <BottomButton type="submit" disabled={isPasswordEmpty || hasErrors}>
             비밀번호 변경하기
           </BottomButton>
         </div>
