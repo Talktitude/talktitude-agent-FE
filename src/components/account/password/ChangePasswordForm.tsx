9019d5f87bee5d386a23c39328a83156e7cb1a0c
ynzung <syj91105@sookmyung.ac.kr>
2025-08-07 14:50:40 +0900

    feat: ChagePasswordForm 컴포넌트 구현 (#41)

---
+++ src/components/account/password/ChangePasswordForm.tsx
@@ -0,0 +1,57 @@
+import InputField from '@/components/auth/InputField';
+import { ChangePasswordFormPropsType } from '@/types/account';
+import BottomButton from '@/components/auth/BottomButton';
+
+const ChangePasswordForm = ({
+  passwordData,
+  onPasswordChange,
+  onChangePasswordSubmit,
+}: ChangePasswordFormPropsType) => {
+  const isPasswordEmpty =
+    !passwordData.currentPassword ||
+    !passwordData.newPassword ||
+    !passwordData.newPasswordConfirm;
+
+  return (
+    <div className="w-full max-w-[420px] mx-auto py-6 h-[calc(100vh-120px)]">
+      <form
+        className="flex flex-col justify-between h-full"
+        onSubmit={onChangePasswordSubmit}
+      >
+        <div className="flex flex-col gap-4">
+          <InputField
+            type="password"
+            value={passwordData.currentPassword}
+            onChange={onPasswordChange('currentPassword')}
+            inputLabel="현재 비밀번호"
+            placeholder="현재 비밀번호를 입력해주세요."
+            isSignup
+          />
+          <InputField
+            type="password"
+            value={passwordData.newPassword}
+            onChange={onPasswordChange('newPassword')}
+            inputLabel="새 비밀번호"
+            placeholder="새 비밀번호를 입력해주세요."
+            isSignup
+          />
+          <InputField
+            type="password"
+            value={passwordData.newPasswordConfirm}
+            onChange={onPasswordChange('newPasswordConfirm')}
+            inputLabel="새 비밀번호 확인"
+            placeholder="새 비밀번호를 다시 입력해주세요."
+            isSignup
+          />
+        </div>
+        <div className="mt-8">
+          <BottomButton type="submit" disabled={isPasswordEmpty}>
+            비밀번호 변경하기
+          </BottomButton>
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default ChangePasswordForm;
