76a568d7010c6cd97e3e531b133ee8b6efc0ef91
ynzung <syj91105@sookmyung.ac.kr>
2025-08-12 16:06:06 +0900

    feat: 커스텀 모달 컴포넌트 type 분리 및 Footer 버튼 컴포넌트 구현 (#43)

--- src/components/common/CustomModal.tsx
+++ src/components/common/CustomModal.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { CustomModalPropsType } from '@/types/account';
 import {
   Dialog,
   DialogContent,
@@ -6,25 +7,21 @@ import {
   DialogTitle,
   DialogOverlay,
   DialogPortal,
+  DialogFooter,
 } from '@/components/ui/dialog';
-
-interface CustomModalProps {
-  open: boolean;
-  onOpenChange: (open: boolean) => void;
-  children: React.ReactNode;
-  mode?: 'center' | 'top-right';
-}
 
 const CustomModal = ({
   open,
   onOpenChange,
   children,
   mode = 'center',
-}: CustomModalProps) => {
+  isFooter = false,
+  onLogout,
+}: CustomModalPropsType) => {
   const getModalClasses = () => {
     switch (mode) {
       case 'top-right':
-        return '!left-auto !top-4 !right-4 !translate-x-0 !translate-y-0 m-0 max-w-md w-full max-h-[calc(100vh-2rem)] overflow-y-auto';
+        return '!left-auto !top-12 !right-16 !translate-x-0 !translate-y-0 m-0 max-w-md w-full max-h-[calc(100vh-2rem)] overflow-y-auto w-[380px]';
       case 'center':
       default:
         return '';
@@ -49,6 +46,16 @@ const CustomModal = ({
           <DialogClose className="absolute right-6 focus:outline-none"></DialogClose>
           <DialogTitle className="sr-only">Modal</DialogTitle>
           {children}
+          {isFooter && (
+            <DialogFooter className="flex flex-col items-center">
+              <button
+                onClick={onLogout}
+                className="text-center text-mainColor font-medium rounded-3xl text-lg py-[16px] w-full hover:bg-gray-50"
+              >
+                로그아웃
+              </button>
+            </DialogFooter>
+          )}
         </DialogContent>
       </DialogPortal>
     </Dialog>
