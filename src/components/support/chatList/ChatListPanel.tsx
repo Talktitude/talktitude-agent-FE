fe9bc5d3f303e749650c9de57379df19c2ee8ae2
ynzung <syj91105@sookmyung.ac.kr>
2025-08-14 14:26:00 +0900

    feat: 채팅 목록 불러오기 api 연동 코드 컴포넌트 연결 (#55)

--- src/components/support/chatList/ChatListPanel.tsx
+++ src/components/support/chatList/ChatListPanel.tsx
@@ -1,10 +1,12 @@
 'use client';
 
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import SectionHeader from '@/components/support/SectionHeader';
 import SearchInput from '@/components/common/SearchInput';
 import ChatList from '@/components/support/chatList/ChatList';
 import type { ChatListItemType } from '@/types/support';
+import { getChatList } from '@/api/support/chatListPanelApi';
+import { PiChats } from 'react-icons/pi';
 
 interface ChatListPanelProps {
   onChatSelect?: (chatItem: ChatListItemType) => void;
@@ -12,25 +14,71 @@ interface ChatListPanelProps {
 
 const ChatListPanel = ({ onChatSelect }: ChatListPanelProps) => {
   const [searchValue, setSearchValue] = useState('');
+  const [selectedChat, setSelectedChat] = useState(-1);
+  const [chatListItems, setChatListItems] = useState<ChatListItemType[]>([]);
+  const [filterOption, setFilterOption] = useState<
+    'ALL' | 'IN_PROGRESS' | 'FINISHED'
+  >('ALL');
 
   const handleSearchChat = () => {
     console.log(searchValue);
     setSearchValue('');
   };
-  const handleChangeChat = (e: React.ChangeEvent<HTMLInputElement>) => {
+
+  const onChangeSearchChat = (e: React.ChangeEvent<HTMLInputElement>) => {
     setSearchValue(e.target.value);
     // console.log(searchValue);
   };
+
+  const handleFilterChange = (filter: 'ALL' | 'IN_PROGRESS' | 'FINISHED') => {
+    setFilterOption(filter);
+  };
+
+  const handleChatSelect = (sessionId: number) => {
+    setSelectedChat(sessionId);
+    const selectedChatItem = chatListItems.find(
+      (chat) => chat.sessionId === sessionId,
+    );
+    if (selectedChatItem && onChatSelect) {
+      onChatSelect(selectedChatItem);
+    }
+  };
+
+  useEffect(() => {
+    const fetchChatList = async () => {
+      const response = await getChatList(filterOption);
+      setChatListItems(response.data);
+    };
+    fetchChatList();
+  }, [filterOption]);
+
   return (
     <div className="flex flex-col w-[30%] gap-1 border-r border-lineGray overflow-y-auto">
       <SectionHeader title="상담 목록" />
-      <SearchInput
-        onSearchClick={() => {
-          handleSearchChat();
-        }}
-        onChange={handleChangeChat}
-      />
-      <ChatList onChatSelect={onChatSelect} />
+      {chatListItems.length === 0 ? (
+        <div className="flex flex-col items-center justify-center h-full gap-2.5">
+          <PiChats size={45} color="#949494" />
+          <p className="text-textLightGray text-base font-medium text-center">
+            고객이 상담을 시작하면 여기에 표시됩니다.
+          </p>
+        </div>
+      ) : (
+        <>
+          <SearchInput
+            onSearchClick={() => {
+              handleSearchChat();
+            }}
+            onChange={onChangeSearchChat}
+          />
+          <ChatList
+            chatListItems={chatListItems}
+            selectedChat={selectedChat}
+            filterOption={filterOption}
+            onFilterChange={handleFilterChange}
+            onChatSelect={handleChatSelect}
+          />
+        </>
+      )}
     </div>
   );
 };
