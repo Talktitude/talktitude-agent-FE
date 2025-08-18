39ed662cab6a6b8ad06f8ecfe3c8a176e98e240e
ynzung <syj91105@sookmyung.ac.kr>
2025-08-18 23:24:53 +0900

    del: 채팅 내역 목데이터 삭제 (#60)

--- src/components/support/chatRoom/ChatRoomPanel.tsx
+++ src/components/support/chatRoom/ChatRoomPanel.tsx
@@ -13,99 +13,13 @@ interface ChatRoomPanelProps {
   setInputMessage?: (message: string) => void;
 }
 
-const MOCK_MESSAGE: MessageType[] = [
-  {
-    messageId: 1,
-    senderType: 'client',
-    original:
-      '안녕하세요! 무엇을 도와드릴까요?안녕하세요! 무엇을 도와드릴까요? 안녕하세요! 무엇을 도와드릴까요?',
-    polite: '안녕하세요! 무엇을 도와드릴까요?',
-    timestamp: '2025-01-01 12:00:00',
-  },
-  {
-    messageId: 2,
-    senderType: 'user',
-    original: '안녕하세요! 무엇을 도와드릴까요?',
-    timestamp: '2025-01-01 12:00:00',
-  },
-  {
-    messageId: 3,
-    senderType: 'client',
-    original:
-      '안녕하세요! 무엇을 도와드릴까요?안녕하세요! 무엇을 도와드릴까요?안녕하세요! 무엇을 도와드릴까요?',
-    timestamp: '2025-01-01 12:00:00',
-  },
-  {
-    messageId: 4,
-    senderType: 'user',
-    original: '안녕하세요! 무엇을 도와드릴까요?',
-    timestamp: '2025-01-01 12:00:00',
-  },
-  {
-    messageId: 5,
-    senderType: 'user',
-    original: '안녕하세요! 무엇을 도와드릴까요?',
-    timestamp: '2025-01-01 12:00:00',
-  },
-  {
-    messageId: 6,
-    senderType: 'client',
-    original: '원문입니다 불손불손불손',
-    polite: '안녕하세요! 무엇을 도와드릴까요?',
-    timestamp: '2025-01-01 12:00:00',
-  },
-  {
-    messageId: 7,
-    senderType: 'client',
-    original: '원문입니다 불손불손불손',
-    polite: '안녕하세요! 무엇을 도와드릴까요?',
-    timestamp: '2025-01-01 12:00:00',
-  },
-  {
-    messageId: 8,
-    senderType: 'client',
-    original: '안녕하세요! 무엇을 도와드릴까요?',
-    timestamp: '2025-01-01 12:00:00',
-  },
-  {
-    messageId: 9,
-    senderType: 'client',
-    original: '안녕하세요! 무엇을 도와드릴까요?',
-    timestamp: '2025-01-01 12:00:00',
-  },
-  {
-    messageId: 10,
-    senderType: 'client',
-    original: '안녕하세요! 무엇을 도와드릴까요?',
-    timestamp: '2025-01-01 12:00:00',
-  },
-  {
-    messageId: 11,
-    senderType: 'user',
-    original: '안녕하세요! 무엇을 도와드릴까요?',
-    timestamp: '2025-01-01 12:00:00',
-  },
-  {
-    messageId: 12,
-    senderType: 'user',
-    original: '안녕하세요! 무엇을 도와드릴까요?',
-    timestamp: '2025-01-01 12:00:00',
-  },
-  {
-    messageId: 13,
-    senderType: 'user',
-    original: '안녕하세요! 무엇을 도와드릴까요?',
-    timestamp: '2025-01-01 12:00:00',
-  },
-];
-
 const ChatRoomPanel = ({
   inputMessage: externalInputMessage,
   setInputMessage: externalSetInputMessage,
 }: ChatRoomPanelProps) => {
   const sessionId = useSearchParams().get('sessionId');
   const [chatInfo, setChatInfo] = useState<ChatHeaderInfoType | null>(null);
-  const [messages, setMessages] = useState(MOCK_MESSAGE);
+  const [messages, setMessages] = useState<SupportMessageType[]>([]);
   const [inputMessage, setInputMessage] = useState('');
 
   // 외부에서 제어하는 경우(추천 답변 선택 시) 내부에서 제어하는 경우(기본 입력 시) 구분하여 값 설정
