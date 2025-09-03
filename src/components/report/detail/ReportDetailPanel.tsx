f082b4dbf3a32adeb0cf1db9e8cd184d4175bc3e
ynzung <syj91105@sookmyung.ac.kr>
2025-09-04 02:01:13 +0900

    feat: 상담 메모 등록 api 연동 코드를 컴포넌트와 연결 (#72)

--- src/components/report/detail/ReportDetailPanel.tsx
+++ src/components/report/detail/ReportDetailPanel.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 import { useParams } from 'next/navigation';
 import { ReportDetailType, MemoCommentType } from '@/types/reports';
 import ReportInfo from './ReportInfo';
@@ -14,6 +14,7 @@ const ReportDetailPanel = () => {
   );
   const reportId = useParams().id;
   const [chatmemoList, setChatmemoList] = useState<MemoCommentType[]>([]);
+  const [sessionId, setSessionId] = useState<number>(0);
   const duringMemo = chatmemoList.filter(
     (memo) => memo.memoPhase === 'DURING_CHAT',
   );
@@ -21,15 +22,16 @@ const ReportDetailPanel = () => {
     (memo) => memo.memoPhase === 'AFTER_CHAT',
   );
 
-  useEffect(() => {
-    const fetchReportDetail = async () => {
-      const response = await getReportDetail(Number(reportId));
-      setReportDetail(response.data);
-      console.log('response.data', response.data);
-      setChatmemoList(response.data?.memos ?? []);
-    };
-    fetchReportDetail();
+  const fetchReportDetail = useCallback(async () => {
+    const response = await getReportDetail(Number(reportId));
+    setReportDetail(response.data);
+    setChatmemoList(response.data?.memos ?? []);
+    setSessionId(response.data?.sessionId ?? -1);
   }, [reportId]);
+
+  useEffect(() => {
+    fetchReportDetail();
+  }, [fetchReportDetail]);
 
   return (
     <div className="w-[calc(50%-60px)] bg-white rounded-[20px] shadow-[0px_0px_10px_0px_rgba(0,0,0,0.25)] mx-6 my-4 p-8 flex flex-col gap-3 justify-between">
@@ -39,6 +41,8 @@ const ReportDetailPanel = () => {
         duringMemoCreateAt={duringMemo[0]?.createdAt}
         duringMemo={duringMemo[0]?.memoText}
         afterMemo={afterMemo}
+        sessionId={sessionId}
+        onRefresh={fetchReportDetail}
       />
     </div>
   );
