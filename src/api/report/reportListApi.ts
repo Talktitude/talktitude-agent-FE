5c745198d8aaeb1b9fec280f43e8b2370b09a00d
ynzung <syj91105@sookmyung.ac.kr>
2025-09-03 15:32:09 +0900

    feat: 날짜별 리포트 목록 조회 API 연동 코드 작성 (#68)

---
+++ src/api/report/reportListApi.ts
@@ -0,0 +1,21 @@
+import axios from 'axios';
+
+const API_URL = process.env.NEXT_PUBLIC_API_BASE_URL;
+
+const accessToken =
+  typeof window !== 'undefined' ? localStorage.getItem('accessToken') : null;
+
+export const getReportList = async (date: string) => {
+  try {
+    const response = await axios.get(`${API_URL}/reports?date=${date}`, {
+      headers: {
+        Authorization: `Bearer ${accessToken}`,
+      },
+    });
+    console.log('리포트 목록', response.data);
+    return response.data;
+  } catch (error) {
+    console.error(error);
+    throw error;
+  }
+};
