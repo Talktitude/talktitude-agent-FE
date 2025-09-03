457656df36e6ed6735cc6779b79f04c3d7d7fb03
ynzung <syj91105@sookmyung.ac.kr>
2025-09-03 10:15:54 +0900

    fix: 상담원 채팅 메시지 originalText → textToShow로 변경 (#66)

--- src/components/support/chatRoom/AgentBubble.tsx
+++ src/components/support/chatRoom/AgentBubble.tsx
@@ -8,7 +8,7 @@ export default function AgentBubble({
   return (
     <div className="flex justify-end">
       <div className="max-w-[70%] px-4 py-1.5 rounded-[20px] flex justify-between text-base font-medium break-words shadow-[0px_0px_10px_0px_rgba(0,0,0,0.25)] bg-mainColor text-white self-end rounded-br-none">
-        {message.originalText}
+        {message.textToShow}
       </div>
     </div>
   );
