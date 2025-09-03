4291b74f7e9ee03929b9b726c9988881c3c05836
ynzung <syj91105@sookmyung.ac.kr>
2025-09-03 22:20:11 +0900

    chore: 상담 이력 조회 관련 type 변수명 createAt -> time으로 변경 (#70)

--- src/types/support.ts
+++ src/types/support.ts
@@ -112,7 +112,7 @@ export interface OrderDetailItemType {
 
 export interface SupportHistoryItemType {
   id: number;
-  createdAt: string;
+  time: string;
   category: string;
   summaryText: string;
 }
