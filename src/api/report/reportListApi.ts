002b6fb6a02e2a95abd205117eb11e75e1154c08
ynzung <syj91105@sookmyung.ac.kr>
2025-09-04 15:34:12 +0900

    feat: 리포트 검색 api 연동 코드 작성 (#76)

--- src/api/report/reportListApi.ts
+++ src/api/report/reportListApi.ts
@@ -19,3 +19,19 @@ export const getReportList = async (date: string) => {
     throw error;
   }
 };
+
+export const getReportListBySearch = async (keyword: string, date: string) => {
+  try {
+    const response = await axios.get(
+      `${API_URL}/reports/search?keyword=${keyword}&date=${date}`,
+      {
+        headers: { Authorization: `Bearer ${accessToken}` },
+      },
+    );
+    // console.log('리포트 검색 목록', response.data);
+    return response.data;
+  } catch (error) {
+    console.error(error);
+    throw error;
+  }
+};
