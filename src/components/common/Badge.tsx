4244f829066a7fb77a880699f6ac222735cb1a78
ynzung <syj91105@sookmyung.ac.kr>
2025-09-05 20:45:46 +0900

    design: Badge 컴포넌트 children 값에 따라 스타일 다르게 설정 (#80)

--- src/components/common/Badge.tsx
+++ src/components/common/Badge.tsx
@@ -6,9 +6,18 @@ interface BadgeProps {
 }
 
 const Badge = ({ children }: BadgeProps) => {
+  const isFinished = children === '종료';
   return (
-    <div className="px-2.5 py-[5px] bg-mainColor rounded-[10px] inline-flex justify-center items-center gap-2.5 overflow-hidden">
-      <div className="justify-start text-white text-xs font-medium">
+    <div
+      className={`px-1.5 py-0.5 rounded-[20px] inline-flex justify-center items-center overflow-hidden ${
+        isFinished ? 'bg-[#F9EEEE]' : 'bg-bgLightBlue'
+      }`}
+    >
+      <div
+        className={`justify-start ${
+          isFinished ? 'text-textRed' : 'text-mainColor'
+        } text-xs font-medium`}
+      >
         {children}
       </div>
     </div>
