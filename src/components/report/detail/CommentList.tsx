256d4d27fa0b24a2e2f67e640bd2b6c0fb30e1a0
ynzung <syj91105@sookmyung.ac.kr>
2025-09-18 14:51:28 +0900

    feat: 상담 메모 리스트 scrollIntoView 구현 (#101)

--- src/components/report/detail/CommentList.tsx
+++ src/components/report/detail/CommentList.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import React from 'react';
+import React, { useRef, useEffect } from 'react';
 import MemoComment from './MemoComment';
 import { MemoCommentType } from '@/types/reports';
 import { MessageSquareMore } from 'lucide-react';
@@ -13,12 +13,30 @@ const CommentList = ({
   onDelete: () => Promise<void>;
 }) => {
   const safeMemoList = Array.isArray(memoList) ? memoList : [];
+
+  const memoListEndRef = useRef<HTMLDivElement | null>(null);
+
+  const scrollToBottom = () => {
+    memoListEndRef.current?.scrollIntoView({ behavior: 'auto' });
+  };
+
+  useEffect(() => {
+    scrollToBottom();
+  }, [memoList]);
+
   return safeMemoList.length > 0 ? (
-    <div className="overflow-y-auto">
-      {safeMemoList.map((memo) => (
-        <MemoComment memoCommentData={memo} key={memo.id} onDelete={onDelete} />
-      ))}
-    </div>
+    <>
+      <div className="overflow-y-auto">
+        {safeMemoList.map((memo) => (
+          <MemoComment
+            memoCommentData={memo}
+            key={memo.id}
+            onDelete={onDelete}
+          />
+        ))}
+      </div>
+      <div ref={memoListEndRef} />
+    </>
   ) : (
     <div className="flex flex-col w-full h-full items-center justify-center gap-2 text-textGray">
       <MessageSquareMore className="w-6 text-textLightGray" />
